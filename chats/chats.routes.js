5c95116b2eab3baee1e53ed47fb56559f8826447
Kali-RADWrld <wrldrad1@gmail.com>
2025-08-05 20:43:03 -0400

    Your commit message

--- chats/chats.routes.js
+++ chats/chats.routes.js
@@ -1,18 +1,12 @@
 // @path: chats/chats.routes.js
 import { Router } from 'express';
-import { extractSock } from '../utils/session.js';
+import { attachSession } from '../../middlewares/attachSession.js';
 import * as ctrl from './chats.controller.js';
 
 const router = Router();
 
-router.use((req, res, next) => {
-  try {
-    req.sock = extractSock(req);
-    next();
-  } catch (err) {
-    res.status(err.status || 500).json({ error: err.message });
-  }
-});
+// Usamos el mismo middleware
+router.use(attachSession);
 
 router.get('/', ctrl.getAllChats);
 router.get('/:jid', ctrl.getChatByJid);
--- message/message.routes.js
+++ message/message.routes.js
@@ -1,18 +1,12 @@
 // @path: message/message.routes.js
 import { Router } from 'express';
-import { extractSock } from '../utils/session.js';
+import { attachSession } from '../../middlewares/attachSession.js';
 import { controllers } from './message.controller.js';
 
 const router = Router();
 
-router.use((req, res, next) => {
-  try {
-    req.sock = extractSock(req);
-    next();
-  } catch (err) {
-    res.status(err.status || 500).json({ error: err.message });
-  }
-});
+// Usamos el middleware común
+router.use(attachSession);
 
 Object.keys(controllers).forEach(action =>
   router.post(`/${action}`, controllers[action])
